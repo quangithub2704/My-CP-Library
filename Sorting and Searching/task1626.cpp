955562a9d0f1fab38f6281709fc1751c808f45c0
Dương Anh Quân <154619282+quangithub2704@users.noreply.github.com>
2026-04-22 20:22:27 +0700

    Update task1626.cpp

--- Sorting and Searching/task1626.cpp
+++ Sorting and Searching/task1626.cpp
@@ -1,1 +1,48 @@
+/*
+  CSES Task 1629
+  ý tưởng là sort theo thời gian kết thúc, vì thời gian kết thúc càng sớm thì càng có thời gian trống nhiều hơn (trực giác bruh)
+*/
+#include "bits/stdc++.h"
+using namespace std;
 
+#define ll long long
+#define pb push_back
+#define ppb pop_back
+#define fi first
+#define se second
+#define all(v) (v).begin(),(v).end()
+
+typedef vector<int> vi;
+typedef set<int> si;
+typedef map<int,int> mii;
+typedef pair<int,int> pii;
+
+const int MAX=2e5+7;
+const int MOD=1e9+7;
+//-----
+int n;
+pii a[MAX];
+//-----
+
+void solve(){
+  cin>>n;
+  for(int i=0;i<n;++i)
+    cin>>a[i].se>>a[i].fi;
+
+  sort(a,a+n);
+
+  int ans=1,pos=0;
+  for(int i=1;i<n;++i)
+    if(a[i].se>=a[pos].fi){
+      ++ans;
+      pos=i;
+    }
+
+  cout<<ans;
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);cout.tie(0);
+  solve();
+}
