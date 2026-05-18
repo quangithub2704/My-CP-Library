777556a2bdc95285cac8c6f470a10954b6121e57
Dương Anh Quân <154619282+quangithub2704@users.noreply.github.com>
2026-05-18 08:58:00 +0700

    Clean up includes and type definitions in task2165.cpp
    Removed unnecessary macros and type definitions.

--- Introductory/task2165.cpp
+++ Introductory/task2165.cpp
@@ -8,24 +8,10 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-#define ll long long
-#define pb push_back
-#define ppb pop_back
-#define fi first
-#define se second
-#define all(v) (v).begin(),(v).end()
-
-typedef vector<int> vi;
-typedef set<int> si;
-typedef map<int,int> mii;
-typedef pair<int,int> pii;
-
-const int MAX=2e5+7;
-const int MOD=1e9+7;
-//-----
+//----------------------------
 int n;
-//-----
 
+//----------------------------
 void toh(int n,int source,int aux,int target){
   if(n==1){
     cout<<source<<' '<<target<<'\n';
